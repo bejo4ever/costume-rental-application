21caea408127b6e35d73cef177b44b9c55229124
miss.putritop <miss.putritop@gmail.com>
2011-12-05 22:59:53 +0000

    Issue 44:	Usecase "Admin" Menambah Kostum (revisi) saya menambahkan HalamanTambahKostum.java , DaftarKostum.java , Kostum.java untuk issue tersebut.

---
+++ sistemsewa/src/java/Entity/DaftarKostum.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author PUTRITOP
+ */
+public class DaftarKostum {
+
+    public void tambahKost(Kostum kostum) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    
+}
