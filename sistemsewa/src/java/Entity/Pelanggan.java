9ec91a040be4f2a4f7159e6f1fb888dcc0937234
miss.putritop <miss.putritop@gmail.com>
2011-12-06 01:42:33 +0000

    Issue 45:	Usecase "Admin" Menambah Pelanggan (revisi). mengupdate kodingan DaftarPelanggan.java DaftarPelanggan.class

--- sistemsewa/src/java/Entity/Pelanggan.java
+++ sistemsewa/src/java/Entity/Pelanggan.java
@@ -6,8 +6,32 @@ package entity;
 
 /**
  *
- * @author a
+ * @author PUTRITOP
  */
 public class Pelanggan {
+
+    public void setKodepelanggan(String kodePelangganHtml) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public void setUsernamepelanggan(String usernamePelangganHtml) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public void setPasswordpelanggan(String passwordPelangganHtml) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public void setNamapelanggan(String namaPelangganHtml) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public void setAlamatpelanggan(String alamatPelangganHtml) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public void setTelponpelanggan(String telponPelangganHtml) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
     
 }
