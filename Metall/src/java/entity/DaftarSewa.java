9fbd89c1e3628599171d3b78fd5aa2e5a0e8711e
fanda.aegis <fanda.aegis@gmail.com>
2011-12-15 07:39:16 +0000

    membuat daftar sewa baaru sesuai issue 31 membuat daftar sewa sesuai issue 37

---
+++ Metall/src/java/entity/DaftarSewa.java
@@ -0,0 +1,137 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import entity.exceptions.NonexistentEntityException;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+/**
+ *
+ * @author a
+ */
+public class DaftarSewa implements Serializable {
+
+    public DaftarSewa() {
+         emf = Persistence.createEntityManagerFactory("MetallPU");
+    }
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(Sewa sewa) {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(sewa);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(Sewa sewa) throws NonexistentEntityException, Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            sewa = em.merge(sewa);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Long id = sewa.getId();
+                if (findSewa(id) == null) {
+                    throw new NonexistentEntityException("The sewa with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(Long id) throws NonexistentEntityException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            Sewa sewa;
+            try {
+                sewa = em.getReference(Sewa.class, id);
+                sewa.getId();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The sewa with id " + id + " no longer exists.", enfe);
+            }
+            em.remove(sewa);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<Sewa> findSewaEntities() {
+        return findSewaEntities(true, -1, -1);
+    }
+
+    public List<Sewa> findSewaEntities(int maxResults, int firstResult) {
+        return findSewaEntities(false, maxResults, firstResult);
+    }
+
+    private List<Sewa> findSewaEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Sewa.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Sewa findSewa(Long id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Sewa.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getSewaCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Sewa> rt = cq.from(Sewa.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
