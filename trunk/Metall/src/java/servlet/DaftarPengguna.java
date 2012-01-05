0767807c0554234f2b53cfed05688e1ba06b1be6
miss.putritop@gmail.com <miss.putritop@gmail.com@3a5ad72b-4cf6-0e54-e218-0925a0f98308>
2012-01-05 01:21:16 +0000

    Issue 51 :	Usecase "Admin" Melihat Daftar Pelanggan. melakukan revisi pada servlet DaftarPengguna.java

---
+++ trunk/Metall/src/java/servlet/DaftarPengguna.java
@@ -0,0 +1,103 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import entity.DaftarPelanggan;
+import entity.Pelanggan;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author a
+ */
+@WebServlet(name = "DaftarPengguna", urlPatterns = {"/daftarpengguna"})
+public class DaftarPengguna extends HttpServlet {
+
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        RequestDispatcher dis = null;
+        HttpSession session = request.getSession();
+        DaftarPelanggan dp = new DaftarPelanggan();
+        Pelanggan p = new Pelanggan();
+        //untuk mendapatkan session dari user yang telah login
+        if (session.getAttribute("sessionusername") != null){
+            String username = (String) session.getAttribute("sessionusername");
+            //melakukan pengecekan untuk memastikan bahwa username telah terdaftar
+            boolean hasilCheck = dp.checkPelanggan(username);
+            if (hasilCheck) {
+                 //mengambil user berdasarkan username dari Daftar User
+                p = dp.getPelangganFromName(username);
+                 //username merupakan admin
+                //if (dp.get == 0) {
+                    //menyimpan daftar pelanggan dalam list
+                    List<Pelanggan> plgn = dp.getPlgn();
+                    request.setAttribute("pelanggan", plgn);
+                    //diarahkan ke halaman daftar pelanggan
+                    dis = request.getRequestDispatcher("daftarpelanggan.jsp");
+                    dis.include(request, response);
+        try {
+            /* TODO output your page here
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet DaftarPengguna</title>");  
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet DaftarPengguna at " + request.getContextPath () + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+             */
+        } finally {            
+            out.close();
+        //}
+    }}}}
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
