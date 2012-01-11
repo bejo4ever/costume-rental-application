f1da36cd3e3c1076c00058768e73dee0c1f6af52
miss.putritop@gmail.com <miss.putritop@gmail.com@3a5ad72b-4cf6-0e54-e218-0925a0f98308>
2012-01-11 16:58:11 +0000

    Issue 52:	Menghapus Pelanggan --> saya melakukan perbaikan untuk entity hapus pelanggan HapusPelanggan2.java . . Maaf ya baru commit skg T___T

---
+++ trunk/Metall/src/java/servlet/HapusPelanggan2.java
@@ -0,0 +1,109 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import entity.Pelanggan;
+import entity.DaftarPelanggan;
+import entity.exceptions.NonexistentEntityException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author a
+ */
+public class HapusPelanggan2 extends HttpServlet {
+
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, NonexistentEntityException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        Long pelangganid = Long.parseLong(request.getParameter("pelangganid"));
+
+        Pelanggan pelanggan = new Pelanggan();
+        RequestDispatcher page = null;
+        DaftarPelanggan dp = new DaftarPelanggan();
+        HttpSession session = request.getSession();
+        try {
+             //remove user
+            dp.deletePelanggan(pelangganid);
+            String message;
+            String pages;
+            RequestDispatcher requestDispatcher =
+                    request.getRequestDispatcher("/successDeleting.jsp");
+            message = "Data pelanggan berhasil dihapus";
+            pages = "listpelanggan2";
+            List<Pelanggan> plgn = dp.getPlgn();
+            request.setAttribute("pelanggan", plgn);
+            request.setAttribute("message", message);
+            request.setAttribute("page", pages);
+            requestDispatcher.forward(request, response);
+        } finally {            
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(HapusPelanggan.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+   
+
+    /** 
+     * Handles the HTTP <code>POST</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (NonexistentEntityException ex) {
+            Logger.getLogger(HapusPelanggan.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    } 
+
+    /** 
+     * Returns a short description of the servlet.
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
